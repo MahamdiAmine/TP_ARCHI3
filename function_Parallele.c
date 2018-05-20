a34c77e11f0b50f61d501f65cd6700ad1a59ccda
Mahamdi <mahamdi@localhost.localdomain>
2018-05-20 18:05:24 -0400

    changing the algo

--- function_Parallele.c
+++ function_Parallele.c
@@ -3,20 +3,24 @@
 //
 #include <stdio.h>
 #include <omp.h>
+#include <math.h>
 #include "function_seq.h"
 
 int isPrime_parallele(int number) {
     int i;
     if (number == 2) return 1;
     if (number % 2 == 0) return 0;
-    for (i = 3; i * i <= number; i += 2)
+    omp_set_num_threads(4);
+    #pragma omp parallel for
+    for (i = 3; i  <= (int)sqrt(number); i += 2){
         if (number % i == 0) return 0;
+    }
     return 1;
 }
 
 void find_N_Prime_Numbers_parallele(int n, int *primeNumbers) {
     int count = 0, i;
-#pragma omp parallel num_threads(4) private (i) shared(count)
+//#pragma omp parallel num_threads(4)
     {
         for (i = 2; count < n; i++) {
             if (isPrime_seq(i)) {
